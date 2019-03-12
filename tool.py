8ea484cb8c7bd440eb73d45dc995a9ce0c6d3d54
System Administrator <root@yumindeMacBook-Pro.local>
2019-03-12 11:05:35 +0800

    add photos

--- tool.py
+++ tool.py
@@ -136,7 +136,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../MinYMY.github.io/tree/master/source/photos/ins.json","w") as fp:
+    with open("./MinYMY.github.io/tree/master/source/photos/ins.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
